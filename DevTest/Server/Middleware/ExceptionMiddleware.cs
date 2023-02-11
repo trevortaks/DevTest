c7b9bc30a7834adb296a066ab38df3cfbdb2e9fd
Trevor Takawira <trevortaks@gmail.com>
2023-02-11 03:14:26 +0200

    Added Exception hndling middleware and response model

---
+++ DevTest/Server/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace DevTest.Server.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IWebHostEnvironment _env;
+
+        public ExceptionMiddleware(RequestDelegate next, IWebHostEnvironment env)
+        {
+            _next = next;
+            _env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                //_logger.LogError($"Something went wrong: {ex}");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.OK;
+            if (_env.IsDevelopment())
+            {
+                var json = JsonSerializer.Serialize<ResponseModel>(new ResponseModel()
+                {
+                    Success = false,
+                    Message = exception.Message
+                });
+                return context.Response.WriteAsync(json);
+            }
+
+            var content = JsonSerializer.Serialize<ResponseModel>(new ResponseModel()
+            {
+                Success = false,
+                Message = "Sorry error occurred, Please try again "
+            });
+
+            return context.Response.WriteAsync(content);
+        }
+    }
+
+}
---
+++ DevTest/Server/ResponseModel.cs
@@ -0,0 +1,21 @@
+﻿namespace DevTest.Server
+{
+    public class ResponseModel
+    {
+        public bool Success { get; set; } = true;
+        public string Message { get; set; }
+
+    }
+
+    public class ResponseModel<T> : ResponseModel
+    {
+        public ResponseModel(T content, bool success, string message)
+        {
+            Content = content;
+            Message = message;
+            Success = success;
+        }
+
+        public T Content { get; set; }
+    }
+}
